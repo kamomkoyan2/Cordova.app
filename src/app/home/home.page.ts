775ab2d64fb1abf85b28cb8b36c45a5e01f2fd9d
kamomkoyan2 <kamkoyan@gmail.com>
2019-06-27 23:38:46 +0400

    music-app

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  row: Array<any> = [];
+  col: Array<any> = [];
 
-  constructor() {}
+  constructor() {
+    this.row = Array.from({length:10},(v,k)=>k+1);
+    this.col = Array.from({length:20},(v,k)=>k+1);
 
+  }
+  counter = 0;
+  changeColor(e){
+    console.log(e)
+    this.counter++;
+    let colors = ["#ff0000","#ff4000","#ff8000","#ffbf00"]
+    if(this.counter==3)
+    {
+      this.counter=0
+    }
+    let randomColor = colors[this.counter]
+    e.target.style.background = randomColor;
+    setTimeout(()=>{e.target.style.background = "black"},200)
+  }
 }
+
